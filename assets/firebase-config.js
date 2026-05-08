41e1a5901f54657f5722fadfa40b2d46a5c81137
diegaless <diego.ayala2@um.es>
2026-05-09 00:54:16 +0200

    Enable Firebase web config

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -2,12 +2,14 @@
 // To enable Google login and per-user cloud saves, replace the placeholders
 // with your Firebase Web App config and set enabled to true.
 window.CV_FIREBASE_CONFIG = {
-  enabled: false,
+  enabled: true,
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    appId: "",
-    messagingSenderId: "",
+    apiKey: "AIzaSyCAgd2JENHeHpcm0scD60itRvprOTnR5iU",
+    authDomain: "cvapp-2538e.firebaseapp.com",
+    projectId: "cvapp-2538e",
+    storageBucket: "cvapp-2538e.firebasestorage.app",
+    messagingSenderId: "861930347575",
+    appId: "1:861930347575:web:2e300096d3f4efc2f7f3b2",
+    measurementId: "G-2JF203YLCK",
   },
 };
